2a4da736fee7e22ee63217e51eb623c179f32b45
David Owen <owend91@gmail.com>
2022-10-05 07:47:18 -0600

    Initial Commit

---
+++ CoreDataTestTeams/CoreDataTestTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestTeamsApp.swift
+//  CoreDataTestTeams
+//
+//  Created by David Owen on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
